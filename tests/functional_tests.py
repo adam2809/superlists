fa305d521b9dae5a68a2ed26d39a50717e0ce9f7
adam2809 <adam.kule@gmail.com>
2018-10-13 15:35:10 +0100

    started building tests in tests/functional_tests.py

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class LearningFT(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def testOpensDjngoWecomePage(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        self.assertIn('TODO',self.browser.title)
+
+
+#Checks if script was run from the command line
+if __name__ == '__main__':
+    unittest.main()
+
+# LearningFT().openBrowser()
+# LearningFT().testOpensDjngoWecomePage()
