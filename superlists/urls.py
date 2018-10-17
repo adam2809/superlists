03d805f87f3b5d455d27ce8fd050590b8163ea04
adam2809 <adam.kule@gmail.com>
2018-10-17 21:44:35 +0100

    Fixed the TypeError by making hamePage a callable(ie a function)

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from lists.views import homePage
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls)
+    url(r'^$', homePage, name='home')
 ]
