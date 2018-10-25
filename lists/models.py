4b8dbd8e354d23dd5a4d23670c4695335bc70f9f
adam2809 <adam.kule@gmail.com>
2018-10-25 21:09:14 +0100

    Added time and daysAhead columns and renamed text to name in DB

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    name = models.TextField(default='')
+    daysAhead = models.IntegerField(default=0)
+    time = models.TextField(default='')
--- lists/tests.py
+++ lists/tests.py
@@ -13,6 +13,10 @@ class HomePageTest(TestCase):
 
     def testPOSTRequest(self):
         response = self.client.post('/', data={'reminder_name':'Buy milk','reminder_days_ahead':'1','reminder_time':'11:00'})
+
+        self.assertEqual(Item.objects.count(),1)
+        self.assertEqual
+
         responseString = response.content.decode()
         self.assertIn('Buy milk',responseString)
         self.assertIn('1',responseString)
@@ -22,17 +26,26 @@ class HomePageTest(TestCase):
 class DBTests(TestCase):
         def testSavingAndRetrievingReminders(self):
             firstItem = Item()
-            firstItem.text = 'Very nice first item'
+            firstItem.name = 'Very nice first item name'
+            firstItem.daysAhead = 5
+            firstItem.time = '00:00'
             firstItem.save()
 
             secondItem = Item()
-            secondItem.text = 'This is my second item'
+            secondItem.name = 'Seconds item name is here'
+            secondItem.daysAhead = 3
+            secondItem.time = '00:01'
             secondItem.save()
 
             savedItems = Item.objects.all()
             self.assertEqual(savedItems.count(),2)
 
             firstSavedItem = savedItems[0]
+            self.assertEqual(firstSavedItem.name,'Very nice first item name')
+            self.assertEqual(firstSavedItem.daysAhead,5)
+            self.assertEqual(firstSavedItem.time,'00:00')
+
             secondSavedItem = savedItems[1]
-            self.assertEqual(firstSavedItem.text,'Very nice first item')
-            self.assertEqual(secondSavedItem.text,'This is my second item')
+            self.assertEqual(secondSavedItem.name,'Seconds item name is here')
+            self.assertEqual(secondSavedItem.daysAhead,3)
+            self.assertEqual(secondSavedItem.time,'00:01')
