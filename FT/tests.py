d51852cfff52609c89e333890701ba3a9a604a6d
psyak14 <psyak14@nottingham.ac.uk>
2019-04-11 20:26:23 +0100

    Refactored FT to make two useful functions be global

--- FT/tests.py
+++ FT/tests.py
@@ -9,41 +9,40 @@ from lists.models import Item
 
 from django.test import LiveServerTestCase
 
-MAX_WAIT = 10
-
-class TestingUtils:
-    def checkIfElementInTable(self, element):
-        remindersTable = self.browser.find_element_by_id('id_reminder_table')
-        rows = remindersTable.find_elements_by_tag_name('tr')
+MAX_WAIT = 10\
 
 
-
-    def createAndWaitNewReminder(self, input):
-        #User creates another reminder
-        while True:
-            waitStart = time.time()
-            try:
-                inputName = self.browser.find_element_by_id('id_new_remider_name')
-                inputDaysAhead = self.browser.find_element_by_id('id_new_remider_days_ahead')
-                inputTime = self.browser.find_element_by_id('id_new_remider_time')
-                submitButton = self.browser.find_element_by_tag_name('button')
-
-                inputName.send_keys(input[0])
-                inputDaysAhead.send_keys(input[1])
-                inputTime.send_keys(input[2])
-
-                #User clicks the submit button to confirm reminder creation
-                submitButton.click()
-            except StaleElementReferenceException as e:
-                if time.time() - waitStart > MAX_WAIT:
-                    raise e
-                print('Waiting for elements to load...')
-                time.sleep(0.2)
-                continue
-            break
+def checkIfElementInTable(element, browser):
+    remindersTable = browser.find_element_by_id('id_reminder_table')
+    rows = remindersTable.find_elements_by_tag_name('tr')
+    return any(row.text == element for row in rows)
 
 
-class NewVisitorTest(LiveServerTestCase,TestingUtils):
+def createAndWaitNewReminder(input, browser):
+    #User creates another reminder
+    while True:
+        waitStart = time.time()
+        try:
+            inputName = browser.find_element_by_id('id_new_remider_name')
+            inputDaysAhead = browser.find_element_by_id('id_new_remider_days_ahead')
+            inputTime = browser.find_element_by_id('id_new_remider_time')
+            submitButton = browser.find_element_by_tag_name('button')
+
+            inputName.send_keys(input[0])
+            inputDaysAhead.send_keys(input[1])
+            inputTime.send_keys(input[2])
+
+            #User clicks the submit button to confirm reminder creation
+            submitButton.click()
+        except StaleElementReferenceException as e:
+            if time.time() - waitStart > MAX_WAIT:
+                raise e
+            print('Waiting for elements to load...')
+            time.sleep(0.2)
+            continue
+        break
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -53,13 +52,12 @@ class NewVisitorTest(LiveServerTestCase,TestingUtils):
         self.browser.quit()
 
 
-
     def testCanCreateReminderMultUsers(self):
         # First user visits the page and creates reminder
         self.browser.get(self.live_server_url)
 
-        self.createAndWaitNewReminder(('Buy milk','1','11:00'))
-        self.assertTrue(self.checkIfElementInTable('1: Buy milk at 11:00 in 1 days'))
+        createAndWaitNewReminder(('Buy milk','1','11:00'),self.browser)
+        self.assertTrue(checkIfElementInTable('1: Buy milk at 11:00 in 1 days',self.browser))
 
         currURL = self.browser.current_url
         self.assertRegex(currURL, '/lists/1')
@@ -71,13 +69,13 @@ class NewVisitorTest(LiveServerTestCase,TestingUtils):
 
         # Second user visits page and creates reminder. They see a different table
         # than the first user
-        self.createAndWaitNewReminder(('Go to class','4','08:55'))
+        createAndWaitNewReminder(('Go to class','4','08:55'),self.browser)
 
         currURL = self.browser.current_url
         self.assertRegex(currURL,'/lists/2')
 
-        self.assertTrue(self.checkIfElementInTable('1: Go to class at 08:55 in 4 days'))
-        self.assertFalse(self.checkIfElementInTable('1: Buy milk at 11:00 in 1 days'))
+        self.assertTrue(checkIfElementInTable('1: Go to class at 08:55 in 4 days',self.browser))
+        self.assertFalse(checkIfElementInTable('1: Buy milk at 11:00 in 1 days',self.browser))
 
 
     def testCanCreateReminderSingleUser(self):
@@ -115,36 +113,39 @@ class NewVisitorTest(LiveServerTestCase,TestingUtils):
         self.assertIn(submitButton.text,'Submit')
 
         #User creates new reminder and sees it in the table
-        self.createAndWaitNewReminder(('Buy milk','1','11:00'))
-        self.assertTrue(self.checkIfElementInTable('1: Buy milk at 11:00 in 1 days'))
+        createAndWaitNewReminder(('Buy milk','1','11:00'),self.browser)
+        self.assertTrue(checkIfElementInTable('1: Buy milk at 11:00 in 1 days',self.browser))
 
         #User creates second and sees it in the table
-        self.createAndWaitNewReminder(('Go to class','4','08:55'))
-        self.assertTrue(self.checkIfElementInTable('2: Go to class at 08:55 in 4 days'))
+        createAndWaitNewReminder(('Go to class','4','08:55'),self.browser)
+        self.assertTrue(checkIfElementInTable('2: Go to class at 08:55 in 4 days',self.browser))
 
 
-class MultUsersSelectingAndAddingToListTests(LiveServerTestCase,TestingUtils):
+class MultUsersSelectingAndAddingToList:
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
         self.browser.get(self.live_server_url)
     #   First user creates a reminder
-        self.createAndWaitNewReminder(('Buy milk','1','11:00'))
-
-        # First user quits
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
+        createAndWaitNewReminder(('Buy milk','1','11:00'),self)
+        restartBrowser()
 
         # Second user visits page and creates reminder.
-        self.createAndWaitNewReminder(('Go to class','4','08:55'))
+        createAndWaitNewReminder(('Go to class','4','08:55'),self)
+        restartBrowser()
 
+
+    def restartBrowser():
         self.browser.quit()
         self.browser = webdriver.Firefox()
         self.browser.get(self.live_server_url)
 
-    def testFirstUserCanSelectListAndAddReminder():
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def testusersCanSelectListAndAddReminder():
         chooseListDropdown = self.browser.find_element_by_id('choose_list_dropdown')
         dropdownOptions = chooseListDropdown.find_elements_by_tag_name('option')
 
@@ -153,3 +154,8 @@ class MultUsersSelectingAndAddingToListTests(LiveServerTestCase,TestingUtils):
         except IndexError:
             self.fail("List of first user not found in dropdown menu")
         option1.click()
+
+        createAndWaitNewReminder(('something','sth','doesnt matter'),self.browser)
+        assertTrue(checkIfElementInTable('2: something at sth in doesnt matter days'),self.browser)
+
+        restartBrowser()
